ac670b47d8fd932339efef2eddb82c129f919b61
Nataly2802 <wendynatalyestevezlopez@gmail.com>
2025-11-26 11:10:24 -0500

    Create inversionCapital.dart

---
+++ lib/inversionCapital.dart
@@ -0,0 +1,10 @@
+void main() {
+
+  double capital = 100000.0;
+  double ganancia = capital * 0.02;
+  double total = capital + ganancia;
+
+  print("Capital inicial: $capital");
+  print("Ganancia despues de un Mes: $ganancia");
+  print("Total despues de un Mes: $total");
+}
