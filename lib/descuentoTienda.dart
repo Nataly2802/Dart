b217ae386c08471d7f1c1a6507bd7ec2749c3e48
Nataly2802 <wendynatalyestevezlopez@gmail.com>
2025-11-26 11:31:02 -0500

    Create descuentoTienda.dart

---
+++ lib/descuentoTienda.dart
@@ -0,0 +1,9 @@
+void main() {
+    double totalCompra = 250000.0;
+    double descuento = totalCompra * 0.15;
+    double totalFinal = totalCompra - descuento;
+
+    print("Total de la Compra: $totalCompra");
+    print("Descuento Aplicado: $descuento");
+    print("Total a pagar despues del descuento: $totalFinal");
+}
