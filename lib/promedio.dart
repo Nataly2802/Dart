6c92645c8bb78feabc0cfeda3b128bb9dbb339c7
Nataly2802 <wendynatalyestevezlopez@gmail.com>
2025-11-26 09:41:46 -0500

    Update promedio.dart

---
+++ lib/promedio.dart
@@ -0,0 +1,7 @@
+void main(){
+  var edad1 = 56;
+  var edad2 = 28;
+  var edad3 = 18;
+  double promedio = (edad1 + edad2 + edad3) / 3;
+  print ("El promedio de Edad de las tres personas es: $promedio ");
+}
