07443c2f9554920373415f1b991c0e6757804122
Nataly2802 <wendynatalyestevezlopez@gmail.com>
2025-11-27 16:25:03 -0500

    Create hospital.dart

---
+++ lib/hospital.dart
@@ -0,0 +1,10 @@
+void main(){
+  double presupuesto = 1000000;
+  double ginecologia = presupuesto * 0.40;
+  double traumatologia = presupuesto * 0.30;
+  double pediatria = presupuesto * 30.0;
+
+  print("Presupuesto para Ginecologia: $ginecologia");
+  print("Presupuesto para Traumatologia: $traumatologia");
+  print("Presupuesto para Pediatria: $pediatria");
+}
