ebd03df2e384d3b32ff6259f5b1a482a2194ba5b
Nataly2802 <wendynatalyestevezlopez@gmail.com>
2025-11-27 15:48:08 -0500

    Create banco.dart

---
+++ lib/banco.dart
@@ -0,0 +1,14 @@
+void main(){
+  int N1 = 45;
+  int N2 = 01;
+  int N3 = 71;
+  int N4 = 68;
+  int N5 = 24;
+  int N6 = 52;
+  int N7 = 25;
+  int N8 = 54;
+
+  int total =  (N1 * 50000) + (N2 * 20000) + (N3 * 10000) + (N4 * 5000) + (N5 * 2000) + (N6 * 1000) + (N7 * 500) + (N8 * 100);
+  print("El total de dinero que hay en el banco es: $total")
+  ;
+}
