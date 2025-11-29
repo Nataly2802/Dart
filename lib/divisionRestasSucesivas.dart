5b60772ebd7804ed04f78102ccfb0a44d330c595
Nataly2802 <wendynatalyestevezlopez@gmail.com>
2025-11-29 16:46:27 -0500

    Create divisionRestasSucesivas.dart

---
+++ lib/divisionRestasSucesivas.dart
@@ -0,0 +1,16 @@
+void main() {
+  int dividendo = 8;
+  int divisor = 2;
+  int cociente = 0;
+  int resto = dividendo;
+
+  while (resto >= divisor) {
+    resto = resto - divisor;
+    cociente++;
+  }
+
+  print("Dividendo: $dividendo");
+  print("Divisor: $divisor");
+  print("Cociente: $cociente");
+  print("Resto: $resto");
+}
