1dd2ded0766d619a7048ed3d95b10f8e1a632cdf
Nataly2802 <wendynatalyestevezlopez@gmail.com>
2025-11-29 15:58:17 -0500

    Create terminodoceavo.dart

---
+++ lib/terminodoceavo.dart
@@ -0,0 +1,10 @@
+void main() {
+  int a1 = 6;
+  int d = 5;
+  int n = 12;
+  int a12 = a1 + (n - 1) * d;
+  int suma12 = (n ~/ 2) * (a1 + a12);
+
+  print("El término 12 es: $a12");
+  print("La suma de los primeros 12 términos es: $suma12");
+}
